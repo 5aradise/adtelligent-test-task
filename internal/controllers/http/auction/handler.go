b8c2985935b401d0d2acff50b3e930931aae37f8
5aradise <rozumovskyi.daniil@lll.kpi.ua>
2025-01-22 23:41:29 +0200

    add auction service and handler

--- internal/controllers/http/auction/handler.go
+++ internal/controllers/http/auction/handler.go
@@ -3,22 +3,26 @@ package auction
 import (
 	"log/slog"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/5aradise/adtelligent-test-task/internal/models"
+	"github.com/5aradise/adtelligent-test-task/pkg/api"
+	"github.com/5aradise/adtelligent-test-task/pkg/middleware"
+	"github.com/5aradise/adtelligent-test-task/pkg/util"
 )
 
 type auctionService interface {
-	GetProfitCreative(sourceId int, maxDuration time.Duration) models.Creative
+	GetProfitCreative(sourceId int, maxDuration time.Duration) (models.Creative, error)
 }
 
 type handler struct {
-	s auctionService
-	l *slog.Logger
+	as auctionService
+	l  *slog.Logger
 }
 
-func New(s auctionService, logger *slog.Logger) *handler {
-	return &handler{s, logger}
+func New(as auctionService, logger *slog.Logger) *handler {
+	return &handler{as, logger}
 }
 
 // /auction?sourceID={source_id}&maxDuration={max_duration}
@@ -27,4 +31,59 @@ func (h *handler) Init(router *http.ServeMux) {
 }
 
 func (h *handler) handleAuction(w http.ResponseWriter, r *http.Request) {
+	const op = "handler.handleAuction"
+	l := h.l.With(
+		slog.String("op", op),
+		slog.String("request_id", middleware.GetRequestID(r)),
+	)
+
+	query := r.URL.Query()
+
+	sourceIDStr := query.Get("sourceID")
+	if sourceIDStr == "" {
+		err := api.WriteError(w, http.StatusBadRequest, "sourceID query is empty")
+		if err != nil {
+			l.Warn("cannot make response", util.SlErr(err))
+		}
+		return
+	}
+	maxDurationStr := query.Get("maxDuration")
+	if maxDurationStr == "" {
+		err := api.WriteError(w, http.StatusBadRequest, "maxDuration query is empty")
+		if err != nil {
+			l.Warn("cannot make response", util.SlErr(err))
+		}
+		return
+	}
+
+	sourceID, err := strconv.Atoi(sourceIDStr)
+	if err != nil {
+		err := api.WriteError(w, http.StatusBadRequest, "sourceID query must be a number")
+		if err != nil {
+			l.Warn("cannot make response", util.SlErr(err))
+		}
+		return
+	}
+	maxDuration, err := time.ParseDuration(maxDurationStr)
+	if err != nil {
+		err := api.WriteError(w, http.StatusBadRequest, "maxDuration query must be valid duration")
+		if err != nil {
+			l.Warn("cannot make response", util.SlErr(err))
+		}
+		return
+	}
+
+	creative, err := h.as.GetProfitCreative(sourceID, maxDuration)
+	if err != nil {
+		err := api.WriteError(w, http.StatusBadRequest, err.Error())
+		if err != nil {
+			l.Warn("cannot make response", util.SlErr(err))
+		}
+		return
+	}
+
+	err = api.WriteJSON(w, http.StatusOK, creative)
+	if err != nil {
+		l.Warn("cannot make response", util.SlErr(err))
+	}
 }
--- internal/services/auction/service.go
+++ internal/services/auction/service.go
@@ -1,13 +1,25 @@
 package auction
 
 import (
+	"context"
+	"errors"
 	"log/slog"
+	"math"
 	"time"
 
 	"github.com/5aradise/adtelligent-test-task/internal/models"
+	"github.com/5aradise/adtelligent-test-task/pkg/util"
+)
+
+var (
+	ErrInactiveSource  = errors.New("inactive source")
+	ErrCreativeUnfound = errors.New("creative unfound")
 )
 
 type auctionStorage interface {
+	GetSourceById(ctx context.Context, id int) (models.Source, error)
+	GetCampaignById(ctx context.Context, id int) (models.Campaign, error)
+	ListCreativesByCampaignId(ctx context.Context, id int) ([]models.Creative, error)
 }
 
 type service struct {
@@ -22,6 +34,60 @@ func New(storage auctionStorage, logger *slog.Logger) *service {
 	}
 }
 
-func (s *service) GetProfitCreative(sourceId int, maxDuration time.Duration) models.Creative {
-	return models.Creative{}
+var maxPriceCreative = models.Creative{Price: math.MaxInt32}
+
+func (s *service) GetProfitCreative(sourceId int, maxDuration time.Duration) (models.Creative, error) {
+	const op = "service.GetProfitCreative"
+	l := s.l.With(
+		slog.String("op", op),
+		slog.Int("source_id", sourceId),
+	)
+
+	source, err := s.stor.GetSourceById(context.Background(), sourceId)
+	if err != nil {
+		return models.Creative{}, util.OpWrap(op, err)
+	}
+
+	if !source.IsActive {
+		return models.Creative{}, util.OpWrap(op, ErrInactiveSource)
+	}
+
+	now := time.Now()
+	bestCreative := maxPriceCreative
+	for _, campaignId := range source.CampaignIds {
+		campaign, err := s.stor.GetCampaignById(context.Background(), campaignId)
+		l = l.With(slog.Int("campaign_id", campaignId))
+		if err != nil {
+			s.l.Warn("cannot get campaign by id", util.SlErr(err))
+			continue
+		}
+
+		if !(campaign.StartTime.Before(now) && campaign.EndTime.After(now)) {
+			s.l.Debug("campaign doesn't fit into time frame", slog.Time("now", now), slog.Time("start", campaign.StartTime), slog.Time("end", campaign.EndTime))
+			continue
+		}
+
+		creatives, err := s.stor.ListCreativesByCampaignId(context.Background(), campaign.ID)
+		if err != nil {
+			s.l.Warn("cannot list creatives by campaign id", util.SlErr(err))
+			continue
+		}
+
+		for _, creative := range creatives {
+			l = l.With(slog.Int("creative_id", creative.ID))
+			if creative.Duration > maxDuration {
+				s.l.Debug("creative takes longer than max duration", slog.Duration("creative_duration", creative.Duration), slog.Duration("max_duration", maxDuration))
+				continue
+			}
+
+			if creative.Price < bestCreative.Price {
+				bestCreative = creative
+			}
+		}
+	}
+
+	if bestCreative == maxPriceCreative {
+		return models.Creative{}, util.OpWrap(op, ErrCreativeUnfound)
+	}
+	return bestCreative, nil
 }
